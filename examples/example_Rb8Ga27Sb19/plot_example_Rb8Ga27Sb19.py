3de37fbe7e96775e8bdad8fe8c775517cdb1c25e
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2024-01-26 09:41:05 -0800

    Removed ground energy from RbGaSb example

--- examples/example_Rb8Ga27Sb19/plot_example_Rb8Ga27Sb19.py
+++ examples/example_Rb8Ga27Sb19/plot_example_Rb8Ga27Sb19.py
@@ -2,4 +2,4 @@ from MCSPS.plotting import plot_swap_trajectory
 from matplotlib import pyplot as plt
 
 # Plot the annealed trajectories
-plot_swap_trajectory('swaps*.out', min_energy=-0.22481528)
+plot_swap_trajectory('swaps*.out')
